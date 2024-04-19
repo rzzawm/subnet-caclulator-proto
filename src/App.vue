d391cc26937c62944a36ce706b42d90d6d97b21f
Reza <rzzawm@gmail.com>
2024-04-19 13:37:57 +0330

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { IP } from './store';
+import SubnetStatus from './components/SubnetStatus.vue';
+import SubnetForm from './components/SubnetForm.vue';
+</script>
+
+<template>
+    <h1>Subnet calculator</h1>
+    <SubnetForm />
+    <SubnetStatus v-if="IP.isValid" />
+</template>
